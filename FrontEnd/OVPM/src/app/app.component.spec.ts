8c2314a429aee4d17cdae7916867204e49343f9a
projektd3us <71829402+projektd3us@users.noreply.github.com>
2022-05-05 04:42:40 +0300

    move

--- FrontEnd/OVPM/src/app/app.component.spec.ts
+++ FrontEnd/OVPM/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'OrbitVisualisationAndPlanetaryMotion'`, () => {
+  it(`should have as title 'OVPM'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('OrbitVisualisationAndPlanetaryMotion');
+    expect(app.title).toEqual('OVPM');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('OrbitVisualisationAndPlanetaryMotion app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('OVPM app is running!');
   });
 });
